0bcb91d410c28628a17c74e62e6ac337d4191b99
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-15 00:59:50 +0700

    [Edit] Ci

--- src/main/java/ml/mixko/shopkoapi/account/Signup.java
+++ src/main/java/ml/mixko/shopkoapi/account/Signup.java
@@ -16,12 +16,12 @@ import java.util.Map;
 @RequestMapping("/account")
 public class Signup {
     @PostMapping("/signup")
-    public Map<String, Object> signup(@RequestParam String firstname, @RequestParam String lastname, @RequestParam String username, @RequestParam String email, @RequestParam String phone, @RequestParam String password){
+    public Map<String, Object> signup(@RequestParam String firstname, @RequestParam String lastname, @RequestParam String username, @RequestParam String phone, @RequestParam String password, @RequestParam String email){
         Map<String, Object> res = new HashMap<>();
 
         try {
             Connection connection = MySQL.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `user` (`id`, `firstname`, `lastname`, `username`, `phone_number`, `gender`, `birthdate`, `password`, `email`, `user_pic`, `type`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL, ?, ?, NULL, 'normal'); ");
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `user` (`id`, `firstname`, `lastname`, `username`, `phone_number`, `gender`, `birthdate`, `password`, `email`, `user_pic`, `type`) VALUES (NULL, ?, ?, ?, ?, NULL, NULL, ?, ?, NULL, 'normal') ");
             preparedStatement.setString(1,firstname);
             preparedStatement.setString(2,lastname);
             preparedStatement.setString(3,username);
