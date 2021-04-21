997d37d694d2584d10aa9cfe021b7a0326e1e35b
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-22 02:04:39 +0700

    [Edit] add /profile

--- src/main/java/ml/mixko/shopkoapi/account/Login.java
+++ src/main/java/ml/mixko/shopkoapi/account/Login.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -61,12 +62,36 @@ public class Login {
             preparedStatement.setInt(1,Integer.parseInt(userid));
             ResultSet rs = preparedStatement.executeQuery();
             rs.next();
+            res.put("username",rs.getString("username"));
             res.put("firstname",rs.getString("firstname"));
             res.put("lastname",rs.getString("lastname"));
-            res.put("username",rs.getString("username"));
             res.put("email",rs.getString("email"));
-            res.put("phone",rs.getString("phone_number"));
+            res.put("profilepic",rs.getString("user_pic"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return res;
+    }
 
+    @PostMapping("/profile")
+    public Map<String, Object> profile(@CookieValue String jwt){
+        Map<String, Object> res = new HashMap<>();
+        String userid;
+        try {
+            userid = JWTUtil.parseToken(jwt);
+            Connection connection = MySQL.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE id = ?");
+            preparedStatement.setInt(1,Integer.parseInt(userid));
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            res.put("username",rs.getString("username"));
+            res.put("firstname",rs.getString("firstname"));
+            res.put("lastname",rs.getString("lastname"));
+            res.put("phone",rs.getString("phone_number"));
+            res.put("email",rs.getString("email"));
+            res.put("gender",rs.getString("gender"));
+            SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd");
+            res.put("birthdate", sdf.format(rs.getDate("birthdate")));
         } catch (Exception e){
             e.printStackTrace();
         }
