233d86aa36e1716e6942db4654558f336fb567cb
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-25 15:11:39 +0700

    [Add] changeGender (ProfileSetting)

--- src/main/java/ml/mixko/shopkoapi/setting/ProfileSetting.java
+++ src/main/java/ml/mixko/shopkoapi/setting/ProfileSetting.java
@@ -74,4 +74,20 @@ public class ProfileSetting {
             e.printStackTrace();
         }
     }
+
+    @PostMapping(path = "/profile/gender")
+    public void changeGender(@CookieValue String jwt, @RequestBody ProfileDTo profile){
+        String userId;
+        try {
+            userId = JWTUtil.parseToken(jwt);
+            Connection connection = MySQL.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE user SET gender = ? WHERE user.id = ?");
+            preparedStatement.setString(1,profile.getGender());
+            preparedStatement.setInt(2,Integer.parseInt(userId));
+            preparedStatement.executeUpdate();
+            System.out.println("Change gender success");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
