7dca69993abb79d021f6001f553cbb6822bf231a
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-04-26 21:51:27 +0700

    [Add] logout and move address to AddressSetting

--- src/main/java/ml/mixko/shopkoapi/account/Login.java
+++ src/main/java/ml/mixko/shopkoapi/account/Login.java
@@ -104,41 +104,16 @@ public class Login {
         return res;
     }
 
-    @PostMapping("/address")
-    public Map<String, Object> address(@CookieValue String jwt){
-        Map<String, Object> res = new HashMap<>();
-        String userid;
+    @PostMapping("/logout")
+    public void logout(HttpServletResponse response){
         try {
-            userid = JWTUtil.parseToken(jwt);
-            Connection connection = MySQL.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM address WHERE user_id = ?");
-            preparedStatement.setInt(1,Integer.parseInt(userid));
-            ResultSet rs = preparedStatement.executeQuery();
-            ArrayList<Map<String,Object>> arrAddress = new ArrayList<>();
-            while (rs.next()) {
-                if (rs.getString("name") == null) {
-                    res.put("address", false);
-                    return res;
-                } else {
-                    Map<String, Object> user = new HashMap<>();
-                    res.put("address", true);
-                    user.put("name", rs.getString("name"));
-                    user.put("province", rs.getString("province"));
-                    user.put("district", rs.getString("district"));
-                    user.put("sub_district", rs.getString("sub_district"));
-                    user.put("post", rs.getString("post"));
-                    user.put("house_number", rs.getString("house_number"));
-                    user.put("details", rs.getString("details"));
-                    user.put("phone", rs.getString("phone_number"));
-                    user.put("id",rs.getInt("id"));
-                    arrAddress.add(user);
-                }
-                res.put("information",arrAddress);
-            }
+            Cookie cookie = new Cookie("jwt",null);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            System.out.println("Logout!");
         } catch (Exception e){
             e.printStackTrace();
         }
-        return res;
     }
 
 }
--- src/main/java/ml/mixko/shopkoapi/setting/AddressSetting.java
+++ src/main/java/ml/mixko/shopkoapi/setting/AddressSetting.java
@@ -7,10 +7,51 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/setting")
 public class AddressSetting {
+    @PostMapping("/address")
+    public Map<String, Object> address(@CookieValue String jwt){
+        Map<String, Object> res = new HashMap<>();
+        String userid;
+        try {
+            userid = JWTUtil.parseToken(jwt);
+            Connection connection = MySQL.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM address WHERE user_id = ?");
+            preparedStatement.setInt(1,Integer.parseInt(userid));
+            ResultSet rs = preparedStatement.executeQuery();
+            ArrayList<Map<String,Object>> arrAddress = new ArrayList<>();
+            while (rs.next()) {
+                if (rs.getString("name") == null) {
+                    res.put("address", false);
+                    return res;
+                } else {
+                    Map<String, Object> user = new HashMap<>();
+                    res.put("address", true);
+                    user.put("name", rs.getString("name"));
+                    user.put("province", rs.getString("province"));
+                    user.put("district", rs.getString("district"));
+                    user.put("sub_district", rs.getString("sub_district"));
+                    user.put("post", rs.getString("post"));
+                    user.put("house_number", rs.getString("house_number"));
+                    user.put("details", rs.getString("details"));
+                    user.put("phone", rs.getString("phone_number"));
+                    user.put("id",rs.getInt("id"));
+                    arrAddress.add(user);
+                }
+                res.put("information",arrAddress);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return res;
+    }
+
     @PostMapping("/address/update")
     public void addressSetting(@RequestBody AddressSettingDTO address){
         try {
