fdb7d234bb85c8fa17ae22fb2a7da5c3cd8755df
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-05-02 22:09:10 +0700

    [Edit] Cart (add delete, add more things in /details)

--- src/main/java/ml/mixko/shopkoapi/shopping/Cart.java
+++ src/main/java/ml/mixko/shopkoapi/shopping/Cart.java
@@ -40,22 +40,46 @@ public class Cart {
         try {
             String id = JWTUtil.parseToken(jwt);
             Connection connection = MySQL.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM cart_item WHERE user_id = ?");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM `cart_item` INNER JOIN product ON product.id = product_id WHERE user_id = ?");
             preparedStatement.setInt(1,Integer.parseInt(id));
             ResultSet rs = preparedStatement.executeQuery();
             ArrayList<Map<String, Object>> arrayList = new ArrayList<>();
             int count = 0;
             System.out.println("Cart counting!");
+            double total = 0;
             while (rs.next()){
                 res.put("isFound",true);
+                Map<String, Object> product = new HashMap<>();
+                product.put("title",rs.getString("title"));
+                product.put("price",rs.getDouble("price"));
+                product.put("quantity_pick",rs.getInt("quantity_pick"));
+                product.put("id",rs.getInt("cart_item.id"));
+                product.put("image",rs.getString("image"));
+                arrayList.add(product);
+                total+=rs.getDouble("price");
                 count++;
             }
             System.out.println(count);
             res.put("productInCart",count);
+            res.put("total",total);
+            res.put("information",arrayList);
         } catch (Exception e){
             res.put("isFound",false);
             e.printStackTrace();
         }
         return res;
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteCart(@RequestParam int id){
+        try {
+            Connection connection = MySQL.getConnection();
+            PreparedStatement preparedStatement =  connection.prepareStatement("DELETE FROM cart_item WHERE id = ?");
+            preparedStatement.setInt(1,id);
+            preparedStatement.execute();
+            System.out.println("Delete item in cart successfully");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
